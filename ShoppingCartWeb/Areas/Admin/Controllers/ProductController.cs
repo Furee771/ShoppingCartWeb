3fb0c12fb3b3cfd21f8e13d5d2b18ba482018290
Furkatzhon <57553056+Furee771@users.noreply.github.com>
2022-04-05 22:47:34 +0700

    установить редактор html файла Create Update tinymce

--- ShoppingCartWeb/Areas/Admin/Controllers/ProductController.cs
+++ ShoppingCartWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ShoppingCart.DataAccess.Repositories;
 using ShoppingCart.DataAccess.ViewModels;
 
@@ -8,26 +9,129 @@ namespace ShoppingCartWeb.Areas.Admin.Controllers
     [Area("Admin")]
      public class ProductController : Controller
      {
-         private IUnitOfWork _unitfWork;
-         private IWebHostEnvironment _hostEnvironment;
-         public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment){
-             _unitfWork = unitOfWork;
-             _hostEnvironment = hostEnvironment;
-         }
-         #region APICALL
+         private IUnitOfWork _unitofWork;
+         private IWebHostEnvironment _hostingEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostingEnvironment)
+        {
+            _unitofWork = unitOfWork;
+            _hostingEnvironment = hostingEnvironment;
+        }
+        #region APICALL
+        public IActionResult AllProducts()
+        {
+            var products = _unitofWork.Product.GetAll(includeProperties: "Category");
+            return Json(new { data = products });
+        }
+        #endregion
 
-         public IActionResult AllProducts()
-         {
-             var products = _unitfWork.Product.GetAll(includeProperties: "Category");
-             return Json(new{data=products});
-             
-         }
-         #endregion
-         public IActionResult Index()
-         {
-             // ProductVM productVM = new ProductVM();
-             // ProductVM.products = _unitOfWork.Product.GetAll();
-             return View();
-         }
-     }
+        public IActionResult Index()
+        {
+            // ProductVM productVM = new ProductVM();
+            // productVM Products = _unitofWork.Product.GetAll();
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateUpdate(int? id)
+        {
+            ProductVM vm = new ProductVM()
+            {
+                Product = new(),
+                Categories = _unitofWork.Category.GetAll().Select(x =>
+                new SelectListItem()
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                })
+
+            };
+            if (id == null || id == 0)
+            {
+                return View(vm);
+            }
+            else
+            {
+                vm.Product = _unitofWork.Product.GetT(x => x.Id == id);
+                if (vm.Product == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(vm);
+                }
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateUpdate(ProductVM vm, IFormFile? file)
+        {
+            if (ModelState.IsValid)
+            {
+                string fileName = String.Empty;
+                if (file != null)
+                {
+                    string uploadDir = Path.Combine(_hostingEnvironment.WebRootPath,
+                        "ProductImage");
+                    fileName = Guid.NewGuid().ToString() + "-" + file.FileName;
+                    string filePath = Path.Combine(uploadDir, fileName);
+
+                    if (vm.Product.ImageUrl != null)
+                    {
+                        var oldImagePath = Path.Combine(_hostingEnvironment.WebRootPath,
+                            vm.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+
+                    }
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    vm.Product.ImageUrl = @"\ProductImage" + fileName;
+                }
+                if (vm.Product.Id == 0)
+                {
+                    _unitofWork.Product.Add(vm.Product);
+                    TempData["success"] = "Product Create Done";
+                }
+                else
+                {
+                    _unitofWork.Product.Update(vm.Product);
+                    TempData["success"] = "Product Update Done!";
+                }
+                _unitofWork.Save();
+
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        #region DeleteAPICALL
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var product = _unitofWork.Product.GetT(x => x.Id == id);
+            if (product == null)
+            {
+                return Json(new { success = false, message = "Error in Fetching Data" });
+            }
+            else
+            {
+                var oldImagePaht = Path.Combine(_hostingEnvironment.WebRootPath, product.ImageUrl.TrimStart('\\')); 
+
+                if (System.IO.File.Exists(oldImagePaht))
+                {
+                    System.IO.File.Delete(oldImagePaht);
+                }
+                _unitofWork.Product.Delete(product);
+                _unitofWork.Save();
+                return Json(new { success = true, message = "Product Deleted" });
+            }
+        }
+        #endregion
+    }
 }
